b61dc66c368dcbabdc00747ecae60e30bed93787
Karen Ling <karensling@gmail.com>
2015-06-29 10:22:53 -0700

    Implement easier selection values.

--- game.rb
+++ game.rb
@@ -39,12 +39,37 @@ class HumanPlayer
   def play_turn(board)
     begin
       puts "#{@name}, what would you like to move?"
-      response_start = gets.chomp
+      response_start = gets.chomp #A7
       puts "#{@name}, where would you like to move to?"
-      response_end = gets.chomp # gives 1, 2
-      response = [response_start.split(",").map(&:to_i), response_end.split(",").map(&:to_i)]
-      start_pos = response[0]
-      end_pos = response[1]
+      response_end = gets.chomp # A5
+
+      convert = {
+        "A" => 0,
+        "B" => 1,
+        "C" => 2,
+        "D" => 3,
+        "E" => 4,
+        "F" => 5,
+        "G" => 6,
+        "H" => 7
+      }
+      start_second = convert[response_start[0]] # A
+      start_first = 8 - response_start[1].to_i # 7
+
+      start_pos = [start_first, start_second]
+      p start_pos
+
+
+      end_second = convert[response_end[0]] # A
+      end_first = 8 - response_end[1].to_i # 7
+
+      end_pos = [end_first, end_second]
+      p end_pos
+
+
+      # response = [response_start.split("").map(&:to_i), response_end.split("").map(&:to_i)]
+      # start_pos = response[0]
+      # end_pos = response[1]
       board.move(start_pos, end_pos)
     rescue ArgumentError => e
       puts e.message
